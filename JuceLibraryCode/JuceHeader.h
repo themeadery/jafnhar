6cbf259479bb34cd499775be5e9bcf9d292986bb
Matt Mead <matt.a.mead@gmail.com>
2026-07-11 06:30:56 -0700

    bump version number for release

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -44,7 +44,7 @@ namespace ProjectInfo
 {
     const char* const  projectName    = "jafnhar";
     const char* const  companyName    = "The Meadery";
-    const char* const  versionString  = "0.0.1";
-    const int          versionNumber  = 0x1;
+    const char* const  versionString  = "0.0.2";
+    const int          versionNumber  = 0x2;
 }
 #endif
